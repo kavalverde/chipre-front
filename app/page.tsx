f61c2fab5b32b798f96f2248620d7836249f2c30
kavalverde <79605050+kavalverde@users.noreply.github.com>
2025-04-16 23:33:06 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,7 @@ import {
 import { ParcelInfo } from "@/components/parcel-info";
 
 const url = `https://geoia.umpacto.com/api/openai/assistant`;
+//const url = `http://localhost:3005/api/openai/assistant`;
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
